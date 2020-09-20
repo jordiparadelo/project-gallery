42a41559b20a4208d34b4db6681a731e5223ef56
Jordi <jparadelo@avantio.com>
2020-09-20 20:20:27 +0200

    Adding styled components and sass set up

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import { projects } from './js/data/data.js';
 
+const elApp = document.querySelector('#app');
+
 // Function that take project color and themed the app
 const colorized = (elements = [], color) => {
     elements.map(el => el.style.setProperty("--colorize", color))
 }
+
 // Function to bind data 
 const bindingFn = () => {
     const listItem = document.querySelector(".project_list");
@@ -24,12 +27,12 @@ const bindingFn = () => {
     };
 
     const imgTemplate = (description, src, id) => {
-        return `<img src="./images/${src}" alt="${description}" class="images_item" data-index="${id}">`
+        return `<img src="./assets/images/${src}" alt="${description}" class="images_item" data-index="${id}">`
     };
 
     // Binding Elements
 
-    listItem.innerHTML = projects.map(({id , projectTitle, projectDescription, color}) => listTemplate(projectTitle, projectDescription, id, color)).join(''); //
+    listItem.innerHTML = projects.map(({ id, projectTitle, projectDescription, color }) => listTemplate(projectTitle, projectDescription, id, color)).join(''); //
     galleryImages.innerHTML = projects.map(({ id, projectDescription, image }) => imgTemplate(projectDescription, image, id)).join('');
     listItem.firstElementChild.dataset.active = "";
 
@@ -37,6 +40,8 @@ const bindingFn = () => {
     colorized([elApp], listItem.firstElementChild.dataset.color);
 };
 
-(function init(){
+// Init Funciton Before Document Ready
+(function init() {
     bindingFn();
 })();
+
